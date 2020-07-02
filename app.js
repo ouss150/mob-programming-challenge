60f0ffdcf265c09290c52c51e6d8e296eb75c8c6
KostraOne <guillaume.amangoua@gmail.com>
2020-07-02 15:12:45 +0200

    added find request to get all users stored in database

--- app.js
+++ app.js
@@ -9,16 +9,22 @@ app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
-
 const images = ["/img/img1.jpg", "/img/img2.jpg", "/img/img3.jpg"];
 
+const UserModel = require("./models/User");
 
 app.get("/", (req, res) => {
   res.render("home", { images });
 });
 
 app.get("/my-dev-squad", (req, res) => {
-  res.render("allUsers", { users });
+  UserModel.find()
+    .then((dbRes) => {
+      res.render("allUsers", { users: dbRes });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 app.get("/add-new-ironhacker", (req, res) => {
