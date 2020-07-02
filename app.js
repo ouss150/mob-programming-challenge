2174ee85d7a614a88d5a96ee0f2edf9af39d5d6f
KostraOne <guillaume.amangoua@gmail.com>
2020-07-02 15:57:20 +0200

    added features CRD

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ require("./config/mongodb");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+
+// define public folder
+app.use(express.static(__dirname + "/public"));
+
+// allow the app to parse the posted data
+app.use(express.urlencoded({ extended: true })); // synchronous
+app.use(express.json()); // async (ajax ready)
+
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
-app.use(express.static(__dirname + "/public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
 const images = ["/img/img1.jpg", "/img/img2.jpg", "/img/img3.jpg"];
@@ -27,9 +35,38 @@ app.get("/my-dev-squad", (req, res) => {
     });
 });
 
+// get will serve the page (with the form)
 app.get("/add-new-ironhacker", (req, res) => {
   res.render("formUser");
 });
+
+// post will receive the informtion from the form
+app.post("/add-new-ironhacker", (req, res) => {
+  console.log(req.body); // will ALLWAYS hold the posted data
+  
+  UserModel.create(req.body)
+  .then(dbRes => {
+    res.send("YAY DATA INSERTED");
+  })
+  .catch(dbErr => {
+    res.send("OH NO AN ERROR OCCURED");
+  })
+  
+});
+
+
+app.get("/delete-ironhacker/:id", (req, res) => {
+  console.log(req.params.id);
+  
+  UserModel.findByIdAndRemove(req.params.id)
+  .then(dbRes => {
+    res.redirect("/my-dev-squad");
+  })
+  .catch(dberror => {
+    res.send("OH NOOOO : ERROR while user removal");
+  })
+})
+
 
 app.get("/api/ironhackers", (req, res) => {
   res.json(users);
